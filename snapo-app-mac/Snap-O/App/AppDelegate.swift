55c928e080bb7c791016dda42e9c4927632fa8ce
Richard Zadorozny <rz@openai.com>
2025-11-17 15:26:19 -0500

    Don't reorder windows when reopening Snap-O (#68)

--- snapo-app-mac/Snap-O/App/AppDelegate.swift
+++ snapo-app-mac/Snap-O/App/AppDelegate.swift
@@ -7,13 +7,6 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     true
   }
 
-  func applicationShouldHandleReopen(_ sender: NSApplication, hasVisibleWindows: Bool) -> Bool {
-    if let window = NSApplication.shared.windows.first {
-      window.makeKeyAndOrderFront(nil)
-    }
-    return true
-  }
-
   func applicationShouldRestoreApplicationState(_ app: NSApplication) -> Bool { false }
   func applicationShouldSaveApplicationState(_ app: NSApplication) -> Bool { false }
 
